85c7e5f0b368c8123d96b1e88d6985d4a9d9e998
fady hany <fadyhanyyy7@gmail.com>
2026-09-10 20:57:47 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,44 +1,31 @@
-const storeData = {
-  // بيانات المحل
-  title: "White for woman",
-  subtitle: "Welcome to my page 👋",
-  logo: "logo.png",
-  backgroundImage: "bg.jpg",
-  
-  // قائمة الأزرار بالروابط الحقيقية
-  links: [
-    {
-      name: "Instagram",
-      url: "https://www.instagram.com/white.for.women?stkn=MTBhenAwZmtvOTR5dg==",
-      icon: "instagram.png",
-      type: "social",
-      platform: "instagram"
-    },
-    {
-      name: "Facebook",
-      url: "https://www.facebook.com/share/1CSEAmQhSR/?mibextid=wwXIfr",
-      icon: "facebook.png",
-      type: "social",
-      platform: "facebook"
-    },
-    {
-      name: "TikTok",
-      url: "https://www.tiktok.com/@white.for.women?_r=1&_t=ZS-99bIeLph8FN",
-      icon: "tik tok.png",
-      type: "social",
-      platform: "tiktok"
-    },
-    {
-      name: "InstaPay",
-      url: "https://ipn.eg/S/moazkishk/instapay/3NfHks",
-      icon: "instapay.png",
-      type: "instapay"
-    }
-  ],
-
-  // حقوق شركة Vodoo
-  developer: {
-    name: "Vodoo",
-    url: "https://www.instagram.com/vodoo_account" // استبدل هذا برابط حساب شركتك Vodoo
-  }
+const config = {
+    title: "White for woman",
+    subtitle: " Welcome to my page",
+    logo: "logo.png",
+    links: [
+        {
+            name: "Instagram",
+            url: "https://www.instagram.com/white.for.women?stkn=MTBhenAwZmtvOTR5dg==",
+            icon: "fa-brands fa-instagram",
+            class: "btn-instagram"
+        },
+        {
+            name: "Facebook",
+            url: "https://www.facebook.com/share/1CSEAmQhSR/?mibextid=wwXIfr",
+            icon: "fa-brands fa-facebook-f",
+            class: "btn-facebook"
+        },
+        {
+            name: "TikTok",
+            url: "https://www.tiktok.com/@white.for.women?_r=1&_t=ZS-99bIeLph8FN",
+            icon: "fa-brands fa-tiktok",
+            class: "btn-tiktok"
+        },
+        {
+            name: "InstaPay",
+            url: "https://ipn.eg/S/moazkishk/instapay/3NfHks",
+            icon: "fa-solid fa-wallet",
+            class: "btn-instapay"
+        }
+    ]
 };
